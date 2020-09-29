1d0c5871e3e22fa5441a55032433017868e39422
ErikGraciosa <erikgraciosa@gmail.com>
2020-09-29 15:58:25 -0700

    passing numbers into eventlistener

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // import functions and grab DOM elements
 // Marry the html tags here:
 
+const buttonSubmit = document.getElementById('button-submit');
+const numberOfTriesLeft = document.getElementById('number-of-tries-left');
+const highOrLow = document.getElementById('high-or-low');
+const winOrLose = document.getElementById('win-or-lose');
+
+
+let initialNumberOfGuesses = parseInt(numberOfTriesLeft.textContent);
 
 
 
@@ -11,3 +18,11 @@
 
 // set event listeners to update state and DOM
 //Click handler here:
+
+buttonSubmit.addEventListener('click', () => {
+    const inputNumber = document.getElementById('input-number');
+    
+    let userGuess = parseInt(inputNumber.value);
+    console.log(typeof userGuess);
+    console.log(typeof initialNumberOfGuesses);
+});
