935dfcbc2016cececd03acd63615f601c826bb0f
ErikGraciosa <erikgraciosa@gmail.com>
2020-09-29 14:45:30 -0700

    html set with requirements from .md file

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // import functions and grab DOM elements
+// Marry the html tags here:
+
+
+
 
 // initialize state
+//Change DOM states here or add variables
+
+
 
 // set event listeners to update state and DOM
+//Click handler here:
